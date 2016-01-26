55432e947bc308dc82ad9c1ddd4b37450e806dcc
pasqualegatto <pasquale.gatto@me.com>
2016-01-26 20:14:15 +0000

    You are building a checkout system for a shop which only sells apples and oranges.

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -4,5 +4,4 @@ import UserPrintOutMessages._
 object Checkout extends App {
 
   initialPrompt
-
 }
--- src/main/scala/UserPrintOutMessages.scala
+++ src/main/scala/UserPrintOutMessages.scala
@@ -1,7 +1,7 @@
 
 object UserPrintOutMessages {
 
-  private val welcomeMessage = "In this shop we only sell apples and oranges"
+  private val welcomeMessage = s"In this shop we only sell apples and oranges\n"
 
   def initialPrompt = println(welcomeMessage)
 }
