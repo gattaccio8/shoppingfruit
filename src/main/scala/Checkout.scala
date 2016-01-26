378218633ca474d79e85ae919886e3ebd2449d20
pasqualegatto <pasquale.gatto@me.com>
2016-01-26 21:45:48 +0000

    For example: [ Apple, Apple, Orange, Apple ] => £2.05

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,7 +1,21 @@
 import UserPrintOutMessages._
+import calculator.FruitCostCalculator
+import model.{Fruit, Orange, Apple}
 
 
 object Checkout extends App {
 
   initialPrompt
+  orderInput
+
+  val order = Console.in.readLine()
+
+  private val fruits: List[Fruit] = order.split(",").map(f => {
+    if (f.contains("Apple")) Apple() else Orange()
+  }).toList
+
+  println(s"You ordered $fruits and the total cost => ${FruitCostCalculator(fruits)}")
+
+  Console.in.close()
+  Console.flush()
 }
